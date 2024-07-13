a168534050353adb29193781cf8851b98565ae3f
acu8 <ewatanabe786@gmail.com>
2024-07-13 15:55:47 +0900

    test env

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,4 @@ import "@testing-library/jest-dom";
 
 // dotenvの設定
 require("dotenv").config();
+
--- jset.config.mjs
+++ jset.config.mjs
@@ -1,7 +1,13 @@
 export default {
-    testEnvironment: "jsdom",
-    moduleNameMapper: {
-      "\\.(css|less)$": "identity-obj-proxy",
-    },
-    setupFilesAfterEnv: ["./jest.setup.js"],
-  };
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@supabase/supabase-js)/)"
+  ],
+};
